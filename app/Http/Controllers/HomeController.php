4233caa504e9c03f8bcc200ab237115fb8830c3e
Kalana Piyumantha <251kalana@gmail.com>
2024-06-20 13:08:47 +0530

    modified

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,7 +11,7 @@ class HomeController extends Controller
 
         if($usertype=='1'){
 
-            return redirect()->route('admin.home');
+            return view('admin.home');
         }
         else{
             return view('user.home');
